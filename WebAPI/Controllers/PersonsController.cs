f3497bca81700e5ef8cab173f9eda16b3d207eab
Phuong Nguyen Duy <ferbious21@gmail.com>
2020-11-13 09:47:55 -0500

    Add project files.

---
+++ WebAPI/Controllers/PersonsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/persons")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly MockPersonRepo _repository = new MockPersonRepo();
+        // Get api/persons
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetAllPersons()
+        {
+            var personItems = _repository.GetPersons();
+            return Ok(personItems);
+        }
+        // Get api/persons/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPersonById(int id)
+        {
+            var personItem = _repository.GetPersonById(id);
+            return Ok(personItem);
+        }
+    }
+
+}
---
+++ WebAPI/Data/MockPersonRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Data
+{
+    public class MockPersonRepo : IPersonRepo
+    {
+        public IEnumerable<Person> GetPersons()
+        {
+            var persons = new List<Person>
+            {
+                new Person { Id = 0, Firstname = "Phuong", Lastname = "Nguyen" },
+                new Person { Id = 1, Firstname = "Lam", Lastname = "Nguyen" },
+                new Person { Id = 2, Firstname = "Hieu", Lastname = "Nguyen" }
+            };
+            return persons;
+        }
+
+        public Person GetPersonById(int id)
+        {
+            return new Person { Id = 0, Firstname = "Phuong", Lastname = "Nguyen" };
+        }
+    }
+}
